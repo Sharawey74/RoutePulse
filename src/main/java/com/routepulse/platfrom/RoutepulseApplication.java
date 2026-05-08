8eca028b5b7e74e84bedd1e7a179496c54616388
Abdelrhman Mohamed <abusharawey2020@gmail.com>
2026-05-08 18:40:35 +0300

    chore(springboot): initial project setup with Maven, pom.xml, and src structure

---
+++ src/main/java/com/routepulse/platfrom/RoutepulseApplication.java
@@ -0,0 +1,13 @@
+package com.routepulse.platfrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutepulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutepulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/routepulse/platfrom/RoutepulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routepulse.platfrom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutepulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
