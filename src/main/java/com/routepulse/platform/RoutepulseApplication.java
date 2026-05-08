5f4df6ef5c8d1a878f23b5e4ff0db5a07a42f436
Abdelrhman Mohamed <abusharawey2020@gmail.com>
2026-05-09 00:00:38 +0300

    fix:Typo errors

--- src/main/java/com/routepulse/platform/RoutepulseApplication.java
+++ src/main/java/com/routepulse/platform/RoutepulseApplication.java
@@ -1,4 +1,4 @@
-package com.routepulse.platfrom;
+package com.routepulse.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/routepulse/platform/RoutepulseApplicationTests.java
+++ src/test/java/com/routepulse/platform/RoutepulseApplicationTests.java
@@ -1,4 +1,4 @@
-package com.routepulse.platfrom;
+package com.routepulse.platform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
